d6248a2dfbfa17f871784f429a3d9f08ffd796b3
mohammad <mohammad200h@hotmail.com>
2024-04-09 10:08:06 +0100

    added few functions to getInfo

---
+++ disjoint_object.py
@@ -0,0 +1,93 @@
+import gmsh
+
+def get_rectangle(center = [0,0],length = 2.0, width = 2.0,mesh_size=0.5):
+
+  # Define the coordinates of the square
+  x_min = center[0] - length / 2
+  x_max = center[0] + length / 2
+  y_min = center[1] - width / 2
+  y_max = center[1] + width / 2
+
+  # Define the points
+  p1 = gmsh.model.geo.addPoint(x_min, y_min, 0 , mesh_size)
+  p2 = gmsh.model.geo.addPoint(x_max, y_min, 0 , mesh_size)
+  p3 = gmsh.model.geo.addPoint(x_max, y_max, 0 , mesh_size)
+  p4 = gmsh.model.geo.addPoint(x_min, y_max, 0 , mesh_size)
+
+  # Define the lines
+  l1 = gmsh.model.geo.addLine(p1, p2)
+  l2 = gmsh.model.geo.addLine(p2, p3)
+  l3 = gmsh.model.geo.addLine(p3, p4)
+  l4 = gmsh.model.geo.addLine(p4, p1)
+
+  # Define the loop
+  loop = gmsh.model.geo.addCurveLoop([l1, l2, l3, l4])
+
+
+  return loop
+
+
+def get_circle(center2D = [0,0],radius = 0.5,mesh_size=0.1):
+
+    # gmsh.model.add("circle_surface")
+
+    # Add points for the inner circle
+    p1 = gmsh.model.geo.addPoint(radius, 0,  0, mesh_size )
+    p2 = gmsh.model.geo.addPoint(0, radius,  0, mesh_size )
+    p3 = gmsh.model.geo.addPoint(-radius, 0, 0, mesh_size)
+    p4 = gmsh.model.geo.addPoint(0, -radius, 0, mesh_size)
+
+    centre = gmsh.model.geo.addPoint(center2D[0], center2D[1], 0, mesh_size, 9)
+
+    # circle arcs
+    arc1 = gmsh.model.geo.addCircleArc(p1, centre, p2)
+    arc2 = gmsh.model.geo.addCircleArc(p2, centre, p3)
+    arc3 = gmsh.model.geo.addCircleArc(p3, centre, p4)
+    arc4 = gmsh.model.geo.addCircleArc(p4, centre, p1)
+
+    loop = gmsh.model.geo.addCurveLoop([arc1,arc2,arc3,arc4])
+
+    return loop
+
+
+def main():
+  ascii = True
+  # Initialize Gmsh ti ASCII
+  if ascii:
+    gmsh.initialize()
+  else:
+     #https://stackoverflow.com/questions/72013304/write-gmsh-mesh-as-binary-file-with-python-api
+     gmsh.initialize(argv=["","-bin"])
+
+  # Create a new model
+  gmsh.model.add("Extrusion")
+
+
+  c1 = get_rectangle(center = [0,0])
+  c2 = get_rectangle(center = [4,4])
+
+  surface_one = gmsh.model.geo.addPlaneSurface([c1])
+  surface_two = gmsh.model.geo.addPlaneSurface([c2])
+
+  height = 2
+  extrusion = gmsh.model.geo.extrude([(2, surface_one)], 0, 0, height)
+  extrusion = gmsh.model.geo.extrude([(2, surface_two)], 0, 0, 2*height)
+
+
+  # Synchronize the geometry
+  gmsh.model.geo.synchronize()
+  # generate mesh
+  gmsh.model.mesh.generate(3)
+
+  # Save the mesh
+  # https://stackoverflow.com/questions/74745628/how-to-save-mesh-in-msh-ascii-2-format-from-gmsh-python-api
+  gmsh.option.setNumber("Mesh.MshFileVersion",4.1)
+  gmsh.write("disjoint_object.msh")
+
+  # Finalize GMSH
+  gmsh.finalize()
+
+
+
+if __name__== "__main__":
+   main()
--- getInfo.py
+++ getInfo.py
@@ -1,24 +1,111 @@
 import gmsh
+from collections import Counter
 import numpy as np
 
 #https://gitlab.onelab.info/gmsh/gmsh/-/blob/master/examples/api/adapt_mesh.py?ref_type=heads
 #https://gitlab.onelab.info/gmsh/gmsh/blob/master/tutorials/python/x7.py
 
+def get_maximum_occurrence(surfaces_index):
+    # https://www.geeksforgeeks.org/python-count-occurrences-element-list/
+    d = Counter(surfaces_index)
+    max_occurrence = 0
+    for s in surfaces_index:
+      occ = d[s]
+      if occ>max_occurrence:
+        max_occurrence = occ
+
+    return max_occurrence
+
+
+def get_point_entities(entities):
+  e_point = []
+  for e in entities:
+    if e[0]==0:
+      e_point.append(e)
+
+  return e_point
+
+def get_curve_entities(entities):
+  e_curve = []
+  for e in entities:
+    if e[0]==1:
+      e_curve.append(e)
+
+  return e_curve
+
+def get_surface_entities(entities):
+  e_surface = []
+  for e in entities:
+    if e[0]==2:
+      e_surface.append(e)
+
+  return e_surface
+
+def get_volume_entities(entities):
+  e_volume = []
+  for e in entities:
+    if e[0]==3:
+      e_volume.append(e)
+
+  return e_volume
+
+def get_boundry_entities_for_volumes(entities):
+  e_volumes = get_volume_entities(entities)
+  e_surface = []
+
+  for e_v in e_volumes:
+    e_surface += gmsh.model.getBoundary([e_v])
+  return e_surface
+
+def is_there_a_shared_surface(entities):
+  e_surface = get_boundry_entities_for_volumes(entities)
+  surface = []
+  for e in e_surface:
+    # shared surface will be repeated but one will have opposite sign
+    surface.append(abs(e[1]))
+
+  occ = get_maximum_occurrence(surface)
+  print(f"occ::{occ}")
+
+  if occ>1:
+     return True
+
+  return False
+
 # Initialize Gmsh
 gmsh.initialize()
 
-gmsh.open('compatible_volume_example.msh')
+gmsh.open('cube_ascii_41.msh')
 
-node_indexes,points,_ = gmsh.model.mesh.getNodes()
+# https://fenicsproject.discourse.group/t/what-does-gmsh-model-getentities-dim-3-s-return-value-represent/12093/2
+entities = gmsh.model.getEntities()
+# print(f"gmsh.model.mesh::gmsh.model.getEntities()::{entities}")
+
+print(f"entites::point::{get_point_entities(entities)}")
+print(f"entites::curve::{get_curve_entities(entities)}")
+print(f"entites::surface::{get_surface_entities(entities)}")
+print(f"entites::volume::{get_volume_entities(entities)}")
+
+for e in get_volume_entities(entities):
+    print(f"boundary::gmsh.model.getBoundary([{e}])::{gmsh.model.getBoundary([e])}")
+
+is_joint = is_there_a_shared_surface(entities)
+
+print(f"is_joint::{is_joint}")
+
+# node_indexes,points,_ = gmsh.model.mesh.getNodes(3,1)
 # print(f"nodes::type::{type(nodes)}")
 # nodes_shape = nodes.reshape(-1,3)
-elements = gmsh.model.mesh.getElements()
+num_nodes,elements_index,elements_nodes_index = gmsh.model.mesh.getElements(3,1)
 
 
 # print(f"node_indexes::{node_indexes}")
 # print(f"node_indexes::shape::{node_indexes.shape[0]}")
 # print(f"points::{points}")
-# print(f"elements::{elements}")
+print(f"num_nodes::{num_nodes}")
+print(f"elements_index::{elements_index}")
+print(f"elements_nodes_index::{np.array(elements_nodes_index).reshape(-1,num_nodes[0])}")
+
 
 triangleElementType = gmsh.model.mesh.getElementType("triangle", 1)
 # print(f"elementType::triangle::{triangleElementType}")
@@ -38,9 +125,9 @@ triangles_tags, evtags = gmsh.model.mesh.getElementsByType(triangleElementType)
 tetrahedron, evtags = gmsh.model.mesh.getElementsByType(tetrahedronElementType)
 
 # print(f"tetrahedron::{tetrahedron}")
-print(f"evtags::{evtags}")
-elements_reshaped = evtags.reshape(-1,4)
-print(f"elements_reshaped::{elements_reshaped}")
+# print(f"evtags::{evtags}")
+# elements_reshaped = evtags.reshape(-1,4)
+# print(f"elements_reshaped::{elements_reshaped}")
 
 # Finalize Gmsh
 gmsh.finalize()
